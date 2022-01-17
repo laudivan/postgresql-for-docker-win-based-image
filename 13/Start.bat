e948bc60218fb26712a7edb13799986e56f745d7
Laudivan F Almeida <laudivan@live.com>
2022-01-17 09:24:29 -0800

    It didn't work on nanoserver image, trying to use servercore one.

--- 13/Start.bat
+++ 13/Start.bat
@@ -1,14 +1,14 @@
-IF EXIST "%PGDATA%\postgresql.conf" (
-    C:\pgsql\bin\pg_ctl.exe --options="-p %PGPORT%" -e PostgreSQL -N PostgreSQL --pgdata="%PGDATA%" start
+IF EXIST "C:\data\postgresql.conf" (
+    C:\pgsql\bin\pg_ctl.exe --options="-p %PGPORT%" -l logfile -e PostgreSQL -N PostgreSQL --pgdata="C:\data" start
 ) ELSE (
-    C:\pgsql\bin\initdb.exe --pgdata="%PGDATA%" --username=%SUPERUSER% --auth=trust
-    C:\pgsql\bin\pg_ctl.exe --options="-p %PGPORT%" -e PostgreSQL -N PostgreSQL --pgdata="%PGDATA%" start
+    C:\pgsql\bin\initdb.exe --encoding=%ENCODING% --locale=%LOCALE% --pgdata="C:\data" --username=%SUPERUSER% --auth=trust
+    C:\pgsql\bin\pg_ctl.exe --options="-p %PGPORT%" -e PostgreSQL -N PostgreSQL --pgdata="C:\data" start
     echo ALTER USER %SUPERUSER% WITH PASSWORD '%SUPERUSERPWD%' ; CREATE EXTENSION adminpack ; | C:\pgsql\bin\psql.exe --username=%SUPERUSER%
 )
 
 REM TODO: Change configs directly on postgresql.conf
 
 
-REM C:\pgsql\bin\postgres.exe -D "%PGDATA%" -p %PGPORT%
+REM C:\pgsql\bin\postgres.exe -D "C:\data" -p %PGPORT%
 
-REM C:\pgsql\bin\pg_ctl.exe start --pgdata="%PGDATA%"
+REM C:\pgsql\bin\pg_ctl.exe start --pgdata="C:\data"
